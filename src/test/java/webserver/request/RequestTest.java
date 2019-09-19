9a2c4af1d8e071de6049ff0287a35c870e6a2c16
thedevluffy <thedevluffy@gmail.com>
2019-09-19 16:34:57 +0900

    test: request 도메인 테스트 추가 및 리팩토링

--- src/test/java/webserver/request/RequestTest.java
+++ src/test/java/webserver/request/RequestTest.java
@@ -11,9 +11,9 @@ import java.nio.charset.StandardCharsets;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class CustomRequestTest {
+class RequestTest {
 
-    private CustomRequest customRequest;
+    private Request request;
 
     @BeforeEach
     void setUp() throws IOException {
@@ -25,53 +25,58 @@ class CustomRequestTest {
         stringBuilder.append("\r\n");
 
         InputStream inputStream = new ByteArrayInputStream(stringBuilder.toString().getBytes(StandardCharsets.UTF_8));
-        customRequest = CustomRequest.of(inputStream);
+        request = Request.of(inputStream);
     }
 
     @DisplayName("Version 을 확인한다.")
     @Test
     void getVersion() {
-        assertThat(customRequest.getVersion()).isEqualTo("HTTP/1.1");
+        assertThat(request.getVersion()).isEqualTo("HTTP/1.1");
     }
 
     @DisplayName("Header 를 확인한다.")
     @Test
     void getHeader() {
-        assertThat(customRequest.getHeader("Accept")).isEqualTo("*/*");
+        assertThat(request.getHeader("Accept")).isEqualTo("*/*");
     }
 
     @DisplayName("Method 를 확인한다.")
     @Test
     void getMethod() {
-        assertThat(customRequest.getMethod()).isEqualTo("GET");
+        assertThat(request.getMethod()).isEqualTo("GET");
     }
 
     @DisplayName("Param 을 확인한다.")
     @Test
     void getParam() {
-        assertThat(customRequest.getParam("userId")).isEqualTo("javajigi");
+        assertThat(request.getParam("userId")).isEqualTo("javajigi");
     }
 
     @DisplayName("Body 를 확인한다.")
     @Test
     void getBody() throws IOException {
         StringBuilder stringBuilder = new StringBuilder();
+
         stringBuilder.append("POST /user/create HTTP/1.1\r\n");
         stringBuilder.append("Host: localhost:8080\r\n");
         stringBuilder.append("Connection: keep-alive\r\n");
+        stringBuilder.append("Content-Length: 93\r\n");
         stringBuilder.append("Accept: */*\r\n");
         stringBuilder.append("\r\n");
         stringBuilder.append("userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net\r\n");
 
         InputStream inputStream = new ByteArrayInputStream(stringBuilder.toString().getBytes(StandardCharsets.UTF_8));
-        CustomRequest customRequestWithContents = CustomRequest.of(inputStream);
+        Request requestWithContents = Request.of(inputStream);
 
-        assertThat(customRequestWithContents.getBody("userId")).isEqualTo("javajigi");
+        assertThat(requestWithContents.getBody("userId")).isEqualTo("javajigi");
+        assertThat(requestWithContents.getBody("password")).isEqualTo("password");
+        assertThat(requestWithContents.getBody("name")).isEqualTo("%EB%B0%95%EC%9E%AC%EC%84%B1");
+        assertThat(requestWithContents.getBody("email")).isEqualTo("javajigi%40slipp.net");
     }
 
     @DisplayName("Path 를 확인한다.")
     @Test
     void getPath() {
-        assertThat(customRequest.getPath()).isEqualTo("/user/create");
+        assertThat(request.getPath()).isEqualTo("/user/create");
     }
 }
